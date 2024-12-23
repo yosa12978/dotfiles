548dae8fb7957a639bf568d2c0044b718fcb1580
yosa12978 <yosa12978@gmail.com>
2024-12-23 23:18:58 +0300

    added telescope bindings

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -1,5 +1,19 @@
 local builtin = require('telescope.builtin')
 local actions = require('telescope.actions')
+local telescope = require('telescope')
+
+telescope.setup({
+    defaults = {
+        mappings = {
+            i = {
+                ["<C-k>"] = actions.move_selection_previous,
+                ["<C-j>"] = actions.move_selection_next,
+                ["<C-l>"] = actions.select_default,
+                ["<esc>"] = actions.close
+            }
+        }
+    }
+})
 
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
@@ -7,5 +21,3 @@ vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("grep > ")})
 end)
 
---vim.keymap.set('i', '<C-k>', actions.move_selection_previous)
---vim.keymap.set('i', '<C-j>', actions.move_selection_next)
--- .config/nvim/plugin/packer_compiled.lua
+++ .config/nvim/plugin/packer_compiled.lua
@@ -74,6 +74,11 @@ end
 time([[try_loadstring definition]], false)
 time([[Defining packer_plugins]], true)
 _G.packer_plugins = {
+  ["coc.nvim"] = {
+    loaded = true,
+    path = "/home/yusuf/.local/share/nvim/site/pack/packer/start/coc.nvim",
+    url = "https://github.com/neoclide/coc.nvim"
+  },
   ["goyo.vim"] = {
     loaded = true,
     path = "/home/yusuf/.local/share/nvim/site/pack/packer/start/goyo.vim",
