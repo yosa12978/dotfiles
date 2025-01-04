4f638af7d94fff2a78ffc29b0213961c342ca768
yosa12978 <yosa12978@gmail.com>
2025-01-04 23:35:50 +0300

    fixed remap nvim

--- .config/nvim/lua/custom/remap.lua
+++ .config/nvim/lua/custom/remap.lua
@@ -1,2 +1,5 @@
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, {})
-vim.keymap.set("n", "<leader>qq", vim.cmd.q, {})
+--vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, {})
+--vim.keymap.set("n", "<leader>qq", vim.cmd.q, {})
+
+vim.api.nvim_set_keymap("n", "<leader>pv", "<cmd>:Ex<CR>", {})
+vim.api.nvim_set_keymap("n", "<leader>qq", "<cmd>:q<CR>", {})
