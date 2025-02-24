705651afc40a324517d91b751d01f5aa72c96226
yosa12978 <yosa12978@gmail.com>
2025-02-24 15:23:46 +0300

    upd

--- .config/nvim/lua/custom/plugins/null_ls.lua
+++ .config/nvim/lua/custom/plugins/null_ls.lua
@@ -2,29 +2,30 @@ local null_ls = require("null-ls")
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 null_ls.setup({
-    ft = "go",
-    sources = {
-        null_ls.builtins.formatting.gofmt,
-        null_ls.builtins.formatting.goimports,
-        null_ls.builtins.formatting.golines,
-        null_ls.builtins.formatting.black,
-        null_ls.builtins.formatting.buf,
-        null_ls.builtins.formatting.terraform_fmt,
-        null_ls.builtins.formatting.opentofu_fmt,
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.isort,
-        null_ls.builtins.formatting.prettier,
-    },
-    on_attach = function(client, bufnr)
-        if client.supports_method("textDocument/formatting") then
-            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-            vim.api.nvim_create_autocmd("BufWritePre", {
-                group = augroup,
-                buffer = bufnr,
-                callback = function()
-                    vim.lsp.buf.format({ bufnr = bufnr })
-                end,
-            })
-        end
-    end,
+	ft = "go",
+	sources = {
+		null_ls.builtins.formatting.gofmt,
+		null_ls.builtins.formatting.goimports,
+		null_ls.builtins.formatting.golines,
+		null_ls.builtins.formatting.black,
+		null_ls.builtins.formatting.buf,
+		null_ls.builtins.formatting.terraform_fmt,
+		null_ls.builtins.formatting.opentofu_fmt,
+		null_ls.builtins.formatting.stylua,
+		null_ls.builtins.formatting.isort,
+		null_ls.builtins.formatting.prettier,
+		null_ls.builtins.formatting.shfmt,
+	},
+	on_attach = function(client, bufnr)
+		if client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					vim.lsp.buf.format({ bufnr = bufnr })
+				end,
+			})
+		end
+	end,
 })
