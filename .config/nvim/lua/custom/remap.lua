242991c66a2f93b408c46b58d8a4771e71c2728e
yosa12978 <yosa12978@gmail.com>
2025-04-15 20:19:55 +0300

    added err message keybinding in neovim

--- .config/nvim/lua/custom/remap.lua
+++ .config/nvim/lua/custom/remap.lua
@@ -3,3 +3,4 @@
 
 vim.api.nvim_set_keymap("n", "<leader>pv", "<cmd>:Ex<CR>", {})
 vim.api.nvim_set_keymap("n", "<leader>qq", "<cmd>:q<CR>", {})
+vim.api.nvim_set_keymap("n", "<leader>e", "<cmd>:lua vim.diagnostic.open_float(0, {scope='line'})<CR>", {})
